ce9bd62005734a1cdd1221e524a4510a44bc31e7
Dimitrios Inglezos <dimitrios.inglezos@accenture.com>
2015-10-08 15:01:15 +0100

    Added feedback form and signout button

--- app/routes.js
+++ app/routes.js
@@ -78,16 +78,20 @@ module.exports = {
 //****--------------------------------------------------------------------------------------------------------*****//
 
  app.get("/:mvpversion", function (req, res) {
-      res.render(req.params.mvpversion+'/start/start_persephone1', {"mvpversion": req.params.mvpversion});
+      res.render(req.params.mvpversion+'/start/start_persephone1', {"mvpversion": req.params.mvpversion,"signout":false});
  });  
-      
+
+app.get("/:mvpversion/feedback", function (req, res) {
+      res.render(req.params.mvpversion+'/feedback', {"mvpversion": req.params.mvpversion,"signout":false});
+ });  
+    
       
 //********************************
 // Start routes
 //********************************
            
 app.get('/:mvpversion/tsandcs', function (req, res) {
-  res.render(req.params.mvpversion+'/start/tsandcs', {"mvpversion": req.params.mvpversion});
+  res.render(req.params.mvpversion+'/start/tsandcs', {"mvpversion": req.params.mvpversion, "signout":false});
 });       
 // End Start routes
       
@@ -97,7 +101,7 @@ app.get('/:mvpversion/tsandcs', function (req, res) {
 //********************************
       
     app.get('/:mvpversion/dashboard-persephone-gaps1', function (req, res) {
-      res.render(req.params.mvpversion+'/dashboard/dashboard-persephone-gaps1', {"mvpversion": req.params.mvpversion});
+      res.render(req.params.mvpversion+'/dashboard/dashboard-persephone-gaps1', {"mvpversion": req.params.mvpversion, "signout":true});
     });
       
       
@@ -108,35 +112,35 @@ app.get('/:mvpversion/tsandcs', function (req, res) {
 //********************************
     
     app.get('/:mvpversion/ni/ni-persephone-full5', function (req, res) {
-      res.render(req.params.mvpversion+'/ni/ni-persephone-full5', {"mvpversion": req.params.mvpversion});
+      res.render(req.params.mvpversion+'/ni/ni-persephone-full5', {"mvpversion": req.params.mvpversion, "signout":true});
     });
       
     app.get('/:mvpversion/ni/ni-persephone-fuller', function (req, res) {
-      res.render(req.params.mvpversion+'/ni/ni-persephone-fuller', {"mvpversion": req.params.mvpversion});
+      res.render(req.params.mvpversion+'/ni/ni-persephone-fuller', {"mvpversion": req.params.mvpversion, "signout":true});
     });      
 
     app.get('/:mvpversion/ni/ni-check', function (req, res) {
-      res.render(req.params.mvpversion+'/ni/ni-check', {"mvpversion": req.params.mvpversion});
+      res.render(req.params.mvpversion+'/ni/ni-check', {"mvpversion": req.params.mvpversion, "signout":true});
     });
       
     app.get('/:mvpversion/ni/basket-toolate/:year', function (req, res) {        
-          res.render(req.params.mvpversion+'/ni/basket-toolate', {"year": req.params.year,"mvpversion": req.params.mvpversion});
+          res.render(req.params.mvpversion+'/ni/basket-toolate', {"year": req.params.year,"mvpversion": req.params.mvpversion, "signout":true});
     });
       
     app.get('/:mvpversion/ni/basket-improve/:year/pay/:amount', function (req, res) {        
           res.render(req.params.mvpversion+'/ni/basket-improve', {"year":        
-            req.params.year,"amount":req.params.amount,"mvpversion": req.params.mvpversion});
+            req.params.year,"amount":req.params.amount,"mvpversion": req.params.mvpversion, "signout":true});
     });
     app.get('/:mvpversion/ni/basket-improve-emp-benefits/:year/pay/:amount/emp/:empAmount/ben/:benefitNum', function (req, res)     {        
           res.render(req.params.mvpversion+'/ni/basket-improve-emp-benefits', {"year":           
                     req.params.year,"amount":req.params.amount,
                     "empAmount": req.params.empAmount, "benefitNum": req.params.benefitNum,"mvpversion": 
-                    req.params.mvpversion});
+                    req.params.mvpversion, "signout":true});
     });      
     
     app.get('/:mvpversion/ni/basket-improve-all/:year/pay/:amount/emp/:empAmount/ben/:benefitNum', function (req, res) {        
           res.render(req.params.mvpversion+'/ni/basket-improve-all', {"year": req.params.year,"amount":req.params.amount,
-            "empAmount": req.params.empAmount, "benefitNum": req.params.benefitNum,"mvpversion": req.params.mvpversion});
+            "empAmount": req.params.empAmount, "benefitNum": req.params.benefitNum,"mvpversion": req.params.mvpversion, "signout":true});
     });         
       
 // End NI routes        
